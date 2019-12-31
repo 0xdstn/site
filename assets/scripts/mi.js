4bb8b0e05e8eb9f23c9cb1519a5e57dfea7b7ab3
Dustin <0xdstn@protonmail.com>
2019-12-31 19:43:59 +0000

    Updated library

--- assets/scripts/mi.js
+++ assets/scripts/mi.js
@@ -36,7 +36,15 @@ const MI = `
 / [l:https://www.amazon.com/dp/1328869334|1984] by George Orwell <code>READING</code>
 ^ The [b:Library] is a list of books  
 & Below is a list of books I've read, separated by year. This list was created as part of my [b:2019] New Years resolution to read more. Books read before [b:2019] are listed together in the [b:Books read prior to 2019] section. I attempted to put them in order read, ascending, but those may be off. All books categorized in a year are in order of when they were read, ascending.
+& [b:Books read in 2020]
+& [i:Goal: 0 of 12]
+# [l:https://www.amazon.com/dp/1573229377|Anger] by Thich Nhat Hanh <code>READING</code>
+# [l:https://www.gutenberg.org/ebooks/21|Aesop's Fables] by Aesop <code>READING</code>
+# [l:https://www.amazon.com/dp/B000FC1JAI|Meditations] by Marcus Aurelius, Gregory Hays <code>READING</code>
+# [l:https://www.amazon.com/dp/B07MJ3TDCZ|Stillness is the Key] by Ryan Holiday <code>READING</code>
+# [l:https://www.amazon.com/dp/B01HNJIJB2|The Daily Stoic] by Ryan Holiday <code>READING</code>
 & [b:Books read in 2019]
+& [i:Goal: 17 of 12] &#x2705;
 # [l:https://www.amazon.com/dp/1607747308|The Life-Changing Magic of Tidying Up] by Marie Kondō
 # [l:https://www.amazon.com/dp/B07C31XJGP|No-Nonsense Buddhism for Beginners] by Noah Rasheta
 # [l:https://www.amazon.com/dp/0857834916|Ikigai] by Yukari Mitsuhashi
@@ -53,10 +61,7 @@ const MI = `
 # [l:https://www.amazon.com/dp/B00VE3J1QG|The Nightmare Collective] by Multiple authors
 # [l:https://www.gutenberg.org/ebooks/7256|The Gift of the Magi] by O. Henry
 # [l:https://www.amazon.com/dp/B00JS59ZK4|Watch Dogs: Dark Clouds] by John Shirley
-# [l:https://www.amazon.com/dp/1573229377|Anger] by Thich Nhat Hanh <code>READING</code>
-# [l:https://www.gutenberg.org/ebooks/21|Aesop's Fables] by Aesop <code>READING</code>
-# [l:https://www.amazon.com/dp/B000FC1JAI|Meditations] by Marcus Aurelius, Gregory Hays <code>READING</code>
-# [l:https://www.gutenberg.org/ebooks/46|A Christmas Carol] by Charles Dickens <code>READING</code>
+# [l:https://www.gutenberg.org/ebooks/46|A Christmas Carol] by Charles Dickens
 & [b:Books read prior to 2019]
 - [l:https://www.amazon.com/dp/0545162076|Harry Potter 1-7] by J.K. Rowling
 - [l:https://www.amazon.com/dp/B002BA83RO|The Dead Zone] by Stephen King

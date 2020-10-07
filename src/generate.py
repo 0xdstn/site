9acdfae3f940bc50210520b1a748268e05355a27
Dustin <0xdstn@protonmail.com>
2020-10-07 22:24:23 +0000

    Removed all javascript, switched to python site generation

---
+++ src/generate.py
@@ -0,0 +1,206 @@
+#!/usr/bin/env python3
+
+import shutil
+import os
+import re
+
+basePath = "/~dustin/"
+outPath = "../"
+
+key = {
+	"&": "p",
+	"1": "h1",
+	"2": "h2",
+	"3": "h3",
+	"4": "h4",
+	"5": "h5",
+	"6": "h6",
+	"-": "ul",
+	"#": "ol",
+	">": "blockquote",
+	"@": "img",
+	"?": "div",
+	"^": "h1",
+	"*": "strong",
+	"_": "em"
+}
+
+tplKey = {
+	"b": "strong",
+	"i": "em",
+	"l": "a",
+	"d": "directory"
+}
+
+dataFile = open("data.txt", "r")
+
+data = {}
+curSection = ""
+curPage = ""
+
+# Read the header
+with open("templates/header.html", "r") as headerFile:
+	header = headerFile.read()
+headerFile.close()
+header = header.replace('[BASEPATH]',basePath)
+
+# Read the footer
+with open("templates/footer.html", "r") as footerFile:
+	footer = footerFile.read()
+footerFile.close()
+
+def tpl(line):
+    matches = re.findall(r'\[([a-z]{1}):(.*?)\]', line)
+    for m in matches:
+        k = m[0]
+        tag = "[{}:{}]".format(m[0],m[1])
+        if k not in tplKey: continue
+        if k == "l":
+            link = m[1].split("|")
+            url = link[0]
+            target = ' target="_blank"' if url.startswith("http") else ""
+            txt = link[1] if len(link) > 1 else link[0]
+            if len(target):
+                line = line.replace(tag,'<a href="{}"{}>{}</a>'.format(url,target,txt))
+            else:
+                line = line.replace(tag,'<a href="{}">{}</a>'.format(basePath+url,txt))
+        elif k == "d":
+            ul = "<ul>"
+            for x in data[m[1]]:
+                page = data[m[1]][x]
+                if x != "index":
+                    ul += '<li><a href="{}">{}</a> {}'.format(page["LINK"],page["NAME"],page["DESC"])
+            ul += "</ul>"
+            line = line.replace(tag,ul)
+        else:
+            line = line.replace(tag, "<{}>{}</{}>".format(tplKey[k],m[1],tplKey[k]))
+
+    return line
+
+def createPage(fileName, section, pg):
+	print("    Creating file: " + fileName + ".html")
+
+	output = header.replace("[TITLE]",data[section][pg]["NAME"])
+
+	page = data[section][pg];
+	lines = page["BODY"];
+
+	inList = False
+	prev = ""
+
+	for l in lines:
+		text = l[2:len(l)]
+		char = l[0:1]
+		last = prev
+		prev = char
+
+		# Raw HTML
+		if char == "!": output += text
+
+		# Key not found or comment
+		if char not in key or char == "/": continue
+
+		# Close the list if we still have one open
+		if char not in ["-","#"] and inList:
+			output += "</%s>" % key[last]
+			inList = False;
+
+		# Lists
+		if char in ["-","#"]:
+			if inList and last != char: output += "</%s>" % key[last]
+			if last != char: output += "<%s>" % key[char]
+			output += "<li>%s</li>" % tpl(text)
+			inList = True
+		# Image
+		elif char == "@":
+			hasLink = "|" in text
+			if hasLink:
+				sp = text.split("|")
+				link = sp[1]
+				text = sp[0]
+				target = ' target="_blank"' if link.startswith("http") else ""
+			if len(target):
+				output += '<a href="{}"{}>'.format(link,target)
+			else:
+				output += '<a href="{}">'.format(basePath+link)
+			output += '<img src="{}">'.format(basePath+link)
+			if hasLink: output += "</a>"
+		# All other tags
+		else:
+			val = "<{}>{}</{}>".format(key[char], tpl(text), key[char])
+			if char in ["*","_"]: val = "<p>%s</p>" % val
+			output += val
+				
+	# Close the list if we still have one open
+	if inList:
+		output += "</%s>" % key[last]
+	output += footer
+
+	f = open(outPath + fileName + ".html", "w")
+	f.write(output)
+	f.close()
+
+for line in dataFile:
+	line = line.strip()
+	if line == "":
+		continue
+
+	if line[0] == "~":
+		pg = line[1:len(line)].split(" : ")
+		url = pg[0].split("/");
+		curSection = url[0];
+		curPage = url[1] if len(url) > 1 else "index"
+
+		if curSection not in data: 
+			data[curSection] = {}
+
+		data[curSection][curPage] = {
+			"NAME": pg[1],
+			"DESC": pg[2],
+			"LINK" : pg[0],
+			"BODY" : []
+		}
+	else:
+		if curPage == "" or curSection == "":
+			continue
+		data[curSection][curPage]["BODY"].append(line)
+
+# Loop through each section
+for section in data:
+	print("[*] SECTION " + section)
+
+	# Recursively remove dir if exists
+	if os.path.isdir(outPath + section):
+		shutil.rmtree(outPath + section)
+
+	# Create the dir
+	print("    Creating dir: " + section);
+	os.mkdir(outPath + section)
+
+	print("")
+
+	# Create html files for this section
+	for page in data[section]:
+		print("[+] PAGE " + page);
+
+		# Index page
+		if page == "index":
+			# Create the html file
+			if section == "index":
+				createPage(page,section,page)
+			else:
+				createPage(section + "/" + page,section,page)
+		else:
+			# Create the dir
+			print("    Creating dir: " + section + "/" + page);
+			os.mkdir(outPath + section + "/" + page)
+
+			# Create the html file
+			createPage(section + "/" + page + "/index",section,page)
+
+		print("");
+
+	print("---");
+	print("");
+
+dataFile.close()
